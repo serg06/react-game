3091a07fc7ec314d81e7fd6681536cac57d2b0c9
Serguei <serkhas@hotmail.com>
2022-02-16 11:22:08 -0500

    move towards mouse

--- src/game/Scene.tsx
+++ src/game/Scene.tsx
@@ -1,6 +1,6 @@
 import {useFrame, useLoader} from '@react-three/fiber';
 import React, {useEffect, useRef, useState} from 'react';
-import {Euler, Mesh, RepeatWrapping, Texture, TextureLoader, Vector3} from 'three';
+import {Euler, Mesh, RepeatWrapping, Texture, TextureLoader, Vector2, Vector3} from 'three';
 import background from '../assets/bg54.jpg';
 import dirt from '../assets/dirt.png';
 import {KeyRules, KeyStates, useKeyState} from 'use-key-state';
@@ -10,61 +10,18 @@ import {RootData, rootSlice} from '../redux/reducers';
 import {useSelector} from 'react-redux';
 import {scaleTexture} from '../utils/three';
 
-enum Direction {
-    UP,
-    DOWN,
-    RIGHT,
-    LEFT
-}
-
-const keyToDirection = {
-    w: Direction.UP,
-    s: Direction.DOWN,
-    a: Direction.LEFT,
-    d: Direction.RIGHT
-};
-
-const directionToVector = {
-    [Direction.UP]: new Vector3(0, 1, 0),
-    [Direction.DOWN]: new Vector3(0, -1, 0),
-    [Direction.LEFT]: new Vector3(-1, 0, 0),
-    [Direction.RIGHT]: new Vector3(1, 0, 0),
-};
-
 export function Scene() {
     const [colorMap, colorMap2] = useLoader<Texture, string[]>(TextureLoader, [
         background,
         dirt
     ]);
 
-    const scale = scaleTexture(colorMap, [10000, 10000]);
+    const scale = scaleTexture(colorMap, [100_000, 100_000]);
 
     const [pos, setPos] = useState<Vector3>(new Vector3(0, 0, 0));
     const [rotate, setRotate] = useState(false);
-    const [direction, setDirection] = useState<Direction | undefined>(undefined);
     // const paused = useSelector<RootData>(state => state.paused);
     const ref = useRef<Mesh>(null!);
-
-    const keys = {
-        w: useKeyPress('w'),
-        a: useKeyPress('a'),
-        s: useKeyPress('s'),
-        d: useKeyPress('d')
-    };
-
-    const esc = useKeyPress('esc');
-
-    useEffect(() => {
-        for (let k of Object.keys(keys)) {
-            const state = keys[k as 'w'];
-            const dir = keyToDirection[k as 'w'];
-            if (state) {
-                setDirection(dir);
-                return;
-            }
-        }
-        setDirection(undefined);
-    }, [keys]);
 
     // useEffect(() => {
     //     if (esc) {
@@ -77,22 +34,12 @@ export function Scene() {
     // const rotation = new Euler(0, 0, 0);
     // const [rotation, setRotation] = useState<Euler>(new Euler(0, 0, 0));
 
-    useFrame(({clock, camera}, delta) => {
-        // if (rotate) {
-        //     ref.current.rotateX(delta * 100 * Math.PI / 180);
-        //     // const r = ref!.current!.rotation;
-        //     // r.set(r.x + delta * 1000 * Math.PI/180, r.y, r.z);
-        // }
-        if (direction !== undefined) {
-            const {position, rotation} = ref.current;
-            const vec = directionToVector[direction];
-            // rotation.set(rotation.x + vec.x * delta * 10, rotation.y + vec.y * delta * 10, rotation.z + vec.z * delta * 10);
-            position.set(position.x + vec.x * delta * 100, position.y + vec.y * delta * 100, position.z + vec.z * delta * 100);
-            camera.position.setX(position.x);
-            camera.position.setY(position.y);
-        } else {
-            // console.log(false);
-        }
+    useFrame(({clock, camera, mouse}, delta) => {
+        const {position} = ref.current;
+        mouse = mouse.length() === 0 ? new Vector2(1, 0) : mouse.normalize();
+        position.set(position.x + mouse.x * delta * 200, position.y + mouse.y * delta * 200, position.z);
+        camera.position.setX(position.x);
+        camera.position.setY(position.y);
     });
 
     colorMap.wrapS = colorMap.wrapT = RepeatWrapping;
